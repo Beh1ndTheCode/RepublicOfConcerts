3362cdd42a4469be6354eb6baa997cdda0a2a814
Stefano-Elisio <stefano.elisio@student.univaq.com>
2022-06-03 13:08:04 +0200

    aggiornamento schermata login

--- src/main/java/it/univaq/disim/oop/roc/controller/LoginController.java
+++ src/main/java/it/univaq/disim/oop/roc/controller/LoginController.java
@@ -1,45 +1,52 @@
 package it.univaq.disim.oop.roc.controller;
 
-import java.util.ResourceBundle;
-
-import javax.print.DocFlavor.URL;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.scene.control.Label;
 
 public class LoginController{
 	
 	@FXML
 	private Label loginErrorLabel;
 	
+	@FXML
 	private ImageView logo, imageView;
 	
-	private Text usarnameText, passwordText;
+	@FXML
+	private Text usernameText, passwordText;
 	
+	@FXML
 	private TextField usernameField;
 	
+	@FXML
 	private PasswordField passwordField;
 	
+	@FXML
 	private Button loginButton;
 	
-	public void initialize(URL url, ResourceBundle rb) {
-		loginButton.disableProperty().bind(usernameField.textProperty().isEmpty().or(passwordField.textProperty().isEmpty()));
+	public void initialize() {
+		loginButton.setDisable(true);
+	}
+	
+	public void blockLoginButton() {
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+		boolean isDisable = username.isEmpty() || password.isEmpty();
+		loginButton.setDisable(isDisable);
 	}
 	
 	public void loginAction(ActionEvent event) {
-	/*	 if(!("docente".equals(usernameField.getText()) && ("docente".equals(passwordField.getText())))) {
+		if(!("docente".equals(usernameField.getText()) && ("docente".equals(passwordField.getText())))) {
 			loginErrorLabel.setText("Username e/o password errati!");
 			} 
 		else {
-				Bisogna caricare la vista successiva
+				//Bisogna caricare la vista successiva
 		}
-	*/
 	}
 }
 	
