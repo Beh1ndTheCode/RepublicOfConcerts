e190a1b98ec6edd587947a5975988cc7e9a59932
Stefano-Elisio <stefano.elisio@student.univaq.com>
2022-08-19 15:56:51 +0200

    inizializzazione AggiungiMedotoController

--- src/main/java/it/univaq/disim/oop/roc/controller/finestre/AggiungiMetodoController.java
+++ src/main/java/it/univaq/disim/oop/roc/controller/finestre/AggiungiMetodoController.java
@@ -1,11 +1,19 @@
 package it.univaq.disim.oop.roc.controller.finestre;
 
 import it.univaq.disim.oop.roc.controller.DataInitializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AggiungiMetodoController implements DataInitializable<Object>{
 
+	@FXML
+	private TextField nomeCartaField, intestatarioField, numeroField, scadenzaField, cvvField, nomeContoField, ibanField, swiftField;
+	
+	@FXML
+	private Button aggiungiCartaButton, aggiungiContoButton;
+	
 	public AggiungiMetodoController() {
-
 	}
 
 }
